acd6ec112e0b6a31357843a6ee2e63b35b33768d
Marca Tatem <marca@me.com>
2022-06-07 18:01:10 -0700

    Update Package@swift-5.2.swift

--- Package@swift-5.2.swift
+++ Package@swift-5.2.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/apple/swift-syntax.git",
-                 .exact("0.50200.0")),
+                 .exact("0.50600.1")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
